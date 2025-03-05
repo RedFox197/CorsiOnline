2db5852c8fe585557f08e56265b1ef50872727b7
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-05 17:23:30 +0100

    Initial Commit

---
+++ src/test/java/com/github/owly7/fantarobot/FantarobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.owly7.fantarobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantarobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
