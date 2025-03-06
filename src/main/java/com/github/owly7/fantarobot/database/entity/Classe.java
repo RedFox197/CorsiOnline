e88e98759ee2fe080db4f72660ffd48515fabe68
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-06 17:00:56 +0100

    gabriel todo

--- src/main/java/com/github/owly7/fantarobot/database/entity/Classe.java
+++ src/main/java/com/github/owly7/fantarobot/database/entity/Classe.java
@@ -3,6 +3,14 @@ package com.github.owly7.fantarobot.database.entity;
 import jakarta.persistence.Entity;
 
 //TODO da implementare gabriel
+// classi nome un elenco di lezioni con date
+/*
+ * lista utenti
+ * nome classe
+ * lista lezioni
+ * 
+ * da fare anche repo, service e controller
+ */
 @Entity
 public class Classe {
 
