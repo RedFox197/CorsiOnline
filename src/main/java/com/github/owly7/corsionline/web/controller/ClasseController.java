4894a304c276d3adc424382c81dbd14ea8f72b99
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-08 15:09:51 +0100

    modificati endpoint e stile rotte classe

--- src/main/java/com/github/owly7/corsionline/web/controller/ClasseController.java
+++ src/main/java/com/github/owly7/corsionline/web/controller/ClasseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -12,38 +13,46 @@ import com.github.owly7.corsionline.database.service.ClasseService;
 import com.github.owly7.corsionline.web.Views;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @CrossOrigin("http://localhost:5173/")
 @RestController
-@RequestMapping("/classe")
+@RequestMapping("api/v1/classi")
 public class ClasseController {
     @Autowired
     private ClasseService classeService;
 
-    @JsonView(Views.Classe.Completa.class)
-    @PostMapping("/save")
-    public Classe save(@RequestBody Classe entity) {
-        return classeService.save(entity);
-    }
-
-    @JsonView(Views.Classe.Completa.class)
-    @GetMapping("/all")
+    @GetMapping
     public List<Classe> findAll() {
         return classeService.findAll();
     }
 
-    @JsonView(Views.Classe.Completa.class)
-    @GetMapping("/get")
-    public Classe findById(@RequestParam Long id) {
+    @GetMapping("/{id}")
+    public Classe findById(@PathVariable Long id) {
         return classeService.findById(id).get();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Classe save(@RequestBody Classe entity) {
+        return classeService.save(entity);
+    }
+
+    // TODO aggiornare service con metodo save
+    @PutMapping("/{id}")
+    public Classe update(@PathVariable Long id, @RequestBody Classe entity) {
+        entity.setId(id);
+        return classeService.save(entity);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
         classeService.deleteById(id);
