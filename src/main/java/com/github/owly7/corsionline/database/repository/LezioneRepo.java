8371617e7c9cb6a11971a9b726152e481751231a
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-14 16:28:36 +0100

    commenti

--- src/main/java/com/github/owly7/corsionline/database/repository/LezioneRepo.java
+++ src/main/java/com/github/owly7/corsionline/database/repository/LezioneRepo.java
@@ -11,10 +11,11 @@ import com.github.owly7.corsionline.database.entity.Lezione;
 
 @Repository
 public interface LezioneRepo extends JpaRepository<Lezione, Long> {
+    // fatti giusto per possibile richiesta, ma non utilizzati
     List<Lezione> findByTitolo(String titolo);
-
     List<Lezione> findByDescrizione(String descrizione);
 
+    //come descritto prendo tutte le lezioni di una classe così sul fronted le mostro direttamente senza filtrare
     @Query("SELECT l FROM Lezione l WHERE l.classe.id = :id")
     List<Lezione> findByClasseId(@Param("id") Long id);
 }
